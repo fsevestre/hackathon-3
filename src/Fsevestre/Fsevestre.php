1bb4391c391dc2fd297ac99c52b3749ece6620a2
fsevestre <aniki.taicho@gmail.com>
2016-12-12 10:06:04 +0100

    Correction

--- src/Fsevestre/Fsevestre.php
+++ src/Fsevestre/Fsevestre.php
@@ -21,5 +21,71 @@ class Fsevestre
 
         return $lines;
     }
+
+    public function getLinesFromFileName2($fileName)
+    {
+        $fileHandle = fopen($fileName, 'r');
+
+        while (false !== $line = fgets($fileHandle)) {
+            yield $line;
+        }
+
+        fclose($fileHandle);
+    }
+
+    public function getLinesFromFileName3($file)
+    {
+        return new LineIterator($file);
+    }
 }
 
+
+class LineIterator implements \Iterator
+{
+    protected $fileHandle;
+    protected $line;
+    protected $i;
+
+    public function __construct($fileName)
+    {
+        if (!$this->fileHandle = fopen($fileName, 'r')) {
+            throw new \RuntimeException('Couldn\'t open file "' . $fileName . '"');
+        }
+    }
+
+    public function rewind()
+    {
+        fseek($this->fileHandle, 0);
+
+        $this->line = fgets($this->fileHandle);
+        $this->i = 0;
+    }
+
+    public function valid()
+    {
+        return false !== $this->line;
+    }
+
+    public function current()
+    {
+        return $this->line;
+    }
+
+    public function key()
+    {
+        return $this->i;
+    }
+
+    public function next()
+    {
+        if (false !== $this->line) {
+            $this->line = fgets($this->fileHandle);
+            $this->i++;
+        }
+    }
+
+    public function __destruct()
+    {
+        fclose($this->fileHandle);
+    }
+}
